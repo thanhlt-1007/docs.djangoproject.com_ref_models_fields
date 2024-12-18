f4b663f4e4e4b79d2bade2b48b8eb69390f77b86
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-18 16:41:53 +0700

    Add is_upperclass method to Student

--- main/apps/choices/factories/student_factory.py
+++ main/apps/choices/factories/student_factory.py
@@ -1,10 +1,11 @@
-import factory
+from factory.django import DjangoModelFactory
+from factory.fuzzy import FuzzyChoice
 
 from main.apps.choices.models import Student
 
 
-class StudentFactory(factory.django.DjangoModelFactory):
-    year_in_school = factory.fuzzy.FuzzyChoice(Student.YEAR_IN_SCHOOL_CHOICES.keys())
+class StudentFactory(DjangoModelFactory):
+    year_in_school = FuzzyChoice(Student.YEAR_IN_SCHOOL_CHOICES.keys())
 
     class Meta:
         model = Student
